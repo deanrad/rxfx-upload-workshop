32ff7f2d4591c454cfa1ba54d97c61e5291c0175
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2023-03-04 01:22:19 -0600

    Compute Queue state in the uploadService, react to it in UI.

--- src/services/upload-service.ts
+++ src/services/upload-service.ts
@@ -1,12 +1,49 @@
-import { createQueueingService, createService } from "@rxfx/service";
+import produce from "immer";
+import { createQueueingService, Action, createService } from "@rxfx/service";
 
-import { FileUploadRequest, UploadProgress } from "../types";
+import { FileUploadRequest, UploadProgress, QueueItem } from "../types";
 import { bus } from "./bus";
 import { observableOfUploadOfFile } from "./upload-impl";
 
 /* prettier-ignore */
-export const uploadService = createQueueingService<FileUploadRequest, UploadProgress, Error>(
+export const uploadService = createQueueingService<FileUploadRequest, UploadProgress, Error, QueueItem[]>(
   "upload",                 // A namespace for events: upload/request
   bus,                      // The bus our listener will be on
-  observableOfUploadOfFile  // The lazy, cancelable, and incremental effect
+  observableOfUploadOfFile, // The lazy, cancelable, and incremental effect,
+  (ACs) => (state: QueueItem[] = [], event: Action<FileUploadRequest>) =>
+  produce(state, (items) => {
+    if (!event?.type) return;
+
+    // Adding to the queue
+    if (ACs.request.match(event)) {
+      const item: QueueItem = {
+        name: event.payload.name,
+        status: "pending",
+        percentComplete: "0"
+      };
+      items.push(item);
+    }
+
+    // Updating progress
+    if (ACs.next.match(event)) {
+        const { loaded, total } = event.payload;
+        const percentComplete = loaded
+          ? String(Math.round((loaded * 100) / total)).padStart(2, "0")
+          : "- ";
+
+          const itemToUpdate =
+          items.find(
+            (item) =>
+              item.name === event.payload?.name && item.status !== "canceled"
+          ) || items[0]
+  
+        itemToUpdate.percentComplete = percentComplete;
+      }
+
+    // Clearing completed
+    if (ACs.complete.match(event)) {
+      // pop one off
+      items.shift();
+    }
+  })  
 );
