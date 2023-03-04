eb49121fb3f4ab141e74044f314b1514085d92c5
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2023-03-04 01:00:25 -0600

    Track upload/complete events in UI state. Be unmount-friendly.

--- src/ui/Queue.tsx
+++ src/ui/Queue.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useService } from "@rxfx/react";
+import { useService, useWhileMounted } from "@rxfx/react";
 import { uploadService } from "../services/upload-service";
 import { EMPTY } from "./Queue.mocks";
 
@@ -9,7 +9,15 @@ export const Queue = () => {
   const { isActive } = useService(uploadService);
   const queue = EMPTY;
 
-  const [numCompleted] = useState(0);
+  const [numCompleted, setNumCompleted] = useState(0);
+
+  useWhileMounted(() => {
+    return uploadService.bus
+      .query(uploadService.actions.complete.match)
+      .subscribe(() => {
+        setNumCompleted((i: number) => i + 1);
+      });
+  });
 
   const icon = isActive ? "⏳" : "";
   const completedText = numCompleted ? `(${numCompleted} completed)` : "";
